8267e07297088c7b854aa0c5b044aeb5c865311f
RiazK <RiazCKhan@gmail.com>
2023-02-10 08:46:22 -0500

    shuffle true-false and mcq

--- src/components/Quiz/cloudComputingQuestions.js
+++ src/components/Quiz/cloudComputingQuestions.js
@@ -3,15 +3,11 @@ const cloudConceptQuestions = [
     category: 1, // Describe Cloud Concepts
     type: "multiple",
     difficulty: "medium",
-    question: "What is Microsoft Azure??",
-    correct_answer: "A cloud computing platform",
-    incorrect_answers: ["E-mail", "A new laptop", "Software"],
-    options: [
-      "A cloud computing platform",
-      "software",
-      "A new laptop",
-      "E-mail",
-    ],
+    question:
+      "In a PaaS environment, the cloud provider maintains the physical infrastructure, physical security, and connection to the internet. They also maintain the operating systems, middleware, development tools, and business intelligence services that make up a cloud solution.",
+    correct_answer: "True",
+    incorrect_answers: ["False"],
+    options: ["True", "False"],
   },
   {
     category: 1, // Describe Cloud Concepts
@@ -26,7 +22,8 @@ const cloudConceptQuestions = [
     category: 1, // Describe Cloud Concepts
     type: "multiple",
     difficulty: "medium",
-    question: "What cloud based service does Azure provide?",
+    question:
+      "WWhich of the following is a cloud based service provided by Azure?",
     correct_answer: "Database hosting",
     incorrect_answers: ["Business networking", "On-Prem", "Database mining"],
     options: [
@@ -40,14 +37,24 @@ const cloudConceptQuestions = [
     category: 1, // Describe Cloud Concepts
     type: "multiple",
     difficulty: "medium",
-    question: "What cloud based service does Azure provide?",
+    question:
+      "Infrastructure as a service (IaaS) is the most flexible category of cloud services, as it provides you the maximum amount of control for your cloud resources.",
+    correct_answer: "True",
+    incorrect_answers: ["False"],
+    options: ["True", "False"],
+  },
+  {
+    category: 1, // Describe Cloud Concepts
+    type: "multiple",
+    difficulty: "medium",
+    question: "Select the cloud based service(s) provided by Azure?",
     correct_answer: "Centralized account management",
-    incorrect_answers: ["Local storage", "Database extraction", "Fun"],
+    incorrect_answers: ["Local storage", "Database extraction", "Nord VPN"],
     options: [
       "Local storage",
       "Database extraction",
       "Centralized account management",
-      "Dragon&#039;s Lair",
+      "Nord VPN",
     ],
   },
   {
@@ -73,7 +80,7 @@ const cloudConceptQuestions = [
     category: 1, // Describe Cloud Concepts
     type: "multiple",
     difficulty: "medium",
-    question: "What is a computing service??",
+    question: "What is an example of a computing service??",
     correct_answer: "Virtual machines",
     incorrect_answers: ["Hardware updates", "Administrative tasks", "Speed"],
     options: [
@@ -87,7 +94,8 @@ const cloudConceptQuestions = [
     category: 1, // Describe Cloud Concepts
     type: "multiple",
     difficulty: "medium",
-    question: "Select all computing service?",
+    question:
+      "Select the most accurate answer representing 'popular' computing service?",
     correct_answer: "Virtual Machines, Storage, Databases, and Networking",
     incorrect_answers: [
       "Data, Points, Expressions, and Lambda",
@@ -124,14 +132,28 @@ const cloudConceptQuestions = [
     category: 1, // Describe Cloud Concepts
     type: "multiple",
     difficulty: "medium",
+    question:
+      "Autoscaling, load balancing, and high availability are just some of the cloud concepts that support performance predictability",
+    correct_answer: "True",
+    incorrect_answers: ["False"],
+    options: ["True, False"],
+  },
+  {
+    category: 1, // Describe Cloud Concepts
+    type: "multiple",
+    difficulty: "medium",
     question: "What is always the responsibility of the cloud service provider",
     correct_answer: "physical datacenter, network, and hosts",
-    incorrect_answers: ["Microsoft", "Edge", "Modelling"],
+    incorrect_answers: [
+      "Security and analytics",
+      "Data modelling and connectivity",
+      "Operating systems, networks, and data",
+    ],
     options: [
-      "Edge",
-      "Modelling",
-      "physical datacenter, network, and hosts",
-      "Microsoft",
+      "Security and analytics",
+      "Data modelling and connectivity",
+      "Physical datacenter, network, and hosts",
+      "Operating systems, networks, and data",
     ],
   },
   {
@@ -148,36 +170,18 @@ const cloudConceptQuestions = [
     category: 1, // Describe Cloud Concepts
     type: "multiple",
     difficulty: "medium",
-    question: "What was the first interactive movie video game?",
-    correct_answer: "Astron Belt",
-    incorrect_answers: ["Dragon&#039;s Lair", "Cube Quest", "M.A.C.H. 3"],
-    options: ["Cube Quest", "M.A.C.H. 3", "Astron Belt", "Dragon&#039;s Lair"],
-  },
-  {
-    category: 1, // Describe Cloud Concepts
-    type: "multiple",
-    difficulty: "medium",
-    question: "What was the first interactive movie video game?",
-    correct_answer: "Astron Belt",
-    incorrect_answers: ["Dragon&#039;s Lair", "Cube Quest", "M.A.C.H. 3"],
-    options: ["Cube Quest", "M.A.C.H. 3", "Astron Belt", "Dragon&#039;s Lair"],
-  },
-  {
-    category: 1, // Describe Cloud Concepts
-    type: "multiple",
-    difficulty: "medium",
     question: "What is the Private Cloud",
     correct_answer: "It is a cloud that is used by a single entity",
     incorrect_answers: [
-      "It is a third part cloud",
-      "it is a highly available cloud",
-      "It is a cloud platform",
+      "It is a third party cloud",
+      "It is a highly available cloud",
+      "It offers economy of scale",
     ],
     options: [
-      "it is a highly available cloud",
-      "It is a cloud platform",
+      "It is a highly available cloud",
+      "It offers economy of scale",
       "Astron Belt",
-      "It is a third part cloud",
+      "It is a third party cloud",
       "It is a cloud that is used by a single entity",
     ],
   },
@@ -189,15 +193,15 @@ const cloudConceptQuestions = [
     correct_answer:
       "A public cloud is built, controlled, and maintained by a third-party cloud provider",
     incorrect_answers: [
-      "A public cloud is built, controlled, by Steve",
-      "A public cloud is built for the backup storage only",
+      "A public cloud is built, and controlled, by Steve",
+      "A public cloud is only built to host backup data",
       "a public cloud is built for a dedicated onsite datacenter",
     ],
     options: [
-      "A public cloud is built for the backup storage only",
+      "A public cloud is built, and controlled, by Steve",
+      "A public cloud is only built to host backup data",
       "a public cloud is built for a dedicated onsite datacenter",
       "A public cloud is built, controlled, and maintained by a third-party cloud provider",
-      "A public cloud is built, controlled, by Steve",
     ],
   },
   {
@@ -209,14 +213,14 @@ const cloudConceptQuestions = [
       "A hybrid cloud is a computing environment that uses both public and private clouds in an inter-connected environment",
     incorrect_answers: [
       "A hybrid cloud is a computing environment that uses fibre optics",
-      "Is always more 50/50 public to private ratio",
+      "Is always 50/50 public to private cloud ratio",
       "Built for improved speed.",
     ],
     options: [
-      "Is always more 50/50 public to private ratio",
+      "A hybrid cloud is a computing environment that uses fibre optics",
+      "Is always 50/50 public to private cloud ratio",
       "Built for improved speed.",
       "A hybrid cloud is a computing environment that uses both public and private clouds in an inter-connected environment",
-      "A hybrid cloud is a computing environment that uses fibre optics",
     ],
   },
   {
@@ -258,10 +262,14 @@ const cloudConceptQuestions = [
     question: "What are the benefits of the consumption-based model?",
     correct_answer:
       "No upfront costs and ability to pay for more resources as needed",
-    incorrect_answers: ["Defined contract", "Defined term", "All of the above"],
+    incorrect_answers: [
+      "Defined contract",
+      "Defined usage limits",
+      "All of the above",
+    ],
     options: [
       "Defined contract",
-      "Defined term",
+      "Defined usage limits",
       "All of the above",
       "No upfront costs and ability to pay for more resources as needed",
     ],
@@ -326,6 +334,16 @@ const cloudConceptQuestions = [
     category: 1, // Describe Cloud Concepts
     type: "multiple",
     difficulty: "medium",
+    question:
+      "Performance predictability focuses on predicting the resources needed to deliver a positive experience for your customers",
+    correct_answer: "True",
+    incorrect_answers: ["False"],
+    options: ["True, False"],
+  },
+  {
+    category: 1, // Describe Cloud Concepts
+    type: "multiple",
+    difficulty: "medium",
     question: "What is High availability?",
     correct_answer:
       "High availability focuses on ensuring maximum availability, regardless of disruptions or events that may occur",
@@ -373,16 +391,6 @@ const cloudConceptQuestions = [
     category: 1, // Describe Cloud Concepts
     type: "multiple",
     difficulty: "medium",
-    question:
-      "Horizontal Scaling is the ability to add or remove VMs or containers?",
-    correct_answer: "True",
-    incorrect_answers: ["False"],
-    options: ["True", "False"],
-  },
-  {
-    category: 1, // Describe Cloud Concepts
-    type: "multiple",
-    difficulty: "medium",
     question: "What are the benefits of the cloud?",
     correct_answer: "all of the above",
     incorrect_answers: ["reliability", "predictability", "availability"],
@@ -390,7 +398,7 @@ const cloudConceptQuestions = [
       "predictability",
       "availability",
       "all of the above",
-      "Dragonreliability",
+      "reliability",
     ],
   },
   {
@@ -425,21 +433,15 @@ const cloudConceptQuestions = [
     category: 1, // Describe Cloud Concepts
     type: "multiple",
     difficulty: "medium",
-    question:
-      "Performance predictability focuses on predicting the resources needed to deliver a positive experience for your customers",
-    correct_answer: "True",
-    incorrect_answers: ["False"],
-    options: ["True, False"],
-  },
-  {
-    category: 1, // Describe Cloud Concepts
-    type: "multiple",
-    difficulty: "medium",
-    question:
-      "Autoscaling, load balancing, and high availability are just some of the cloud concepts that support performance predictability",
-    correct_answer: "True",
-    incorrect_answers: ["False"],
-    options: ["True, False"],
+    question: "What is Microsoft Azure??",
+    correct_answer: "A cloud computing platform",
+    incorrect_answers: ["E-mail", "A new laptop", "Software"],
+    options: [
+      "A cloud computing platform",
+      "software",
+      "A new laptop",
+      "E-mail",
+    ],
   },
   {
     category: 1, // Describe Cloud Concepts
@@ -460,15 +462,25 @@ const cloudConceptQuestions = [
       "set templates help ensure that all your deployed resources meet corporate standards ",
       "set templates help ensure that all your deployed resources meet governmemt regulatory requirements",
       "update all your deployed resources to new standards as standards change",
-      "Cloud-based auditing helps flag any resource that’s out of compliance with your corporate standards and provides mitigation strategies",
+      "cloud-based auditing helps flag any resource that’s out of compliance with your corporate standards and provides mitigation strategies",
     ],
     options: [
       "set templates help ensure that all your deployed resources meet corporate standards ",
       "set templates help ensure that all your deployed resources meet governmemt regulatory requirements",
       "update all your deployed resources to new standards as standards change",
-      "Cloud-based auditing helps flag any resource that’s out of compliance with your corporate standards and provides mitigation strategies",
+      "cloud-based auditing helps flag any resource that’s out of compliance with your corporate standards and provides mitigation strategies",
       "All of the above",
     ],
+  },
+  {
+    category: 1, // Describe Cloud Concepts
+    type: "multiple",
+    difficulty: "medium",
+    question:
+      "Management of the cloud speaks to managing your cloud resources. In the cloud, you can: Automatically scale resource deployment based on need. Deploy resources based on a preconfigured template, removing the need for manual configuration. Monitor the health of resources and automatically replace failing resources. Receive automatic alerts based on configured metrics, so you’re aware of performance in real-time.",
+    correct_answer: "True",
+    incorrect_answers: ["False"],
+    options: ["False", "True"],
   },
   {
     category: 1, // Describe Cloud Concepts
@@ -490,6 +502,15 @@ const cloudConceptQuestions = [
     category: 1, // Describe Cloud Concepts
     type: "multiple",
     difficulty: "medium",
+    question: "SaaS is the most flexible cloud model",
+    correct_answer: "True",
+    incorrect_answers: ["False"],
+    options: ["False", "True"],
+  },
+  {
+    category: 1, // Describe Cloud Concepts
+    type: "multiple",
+    difficulty: "medium",
     question: "Which item is not a part of Management in the Cloud?",
     correct_answer: "Azure One Interface",
     incorrect_answers: ["Web Portal", "CLI", "APIs", "PowerShell"],
@@ -500,20 +521,10 @@ const cloudConceptQuestions = [
     type: "multiple",
     difficulty: "medium",
     question:
-      "Management of the cloud speaks to managing your cloud resources. In the cloud, you can: Automatically scale resource deployment based on need",
+      "Management of the cloud speaks to managing your cloud resources. In the cloud, you can: Automatically scale resource deployment based on need.",
     correct_answer: "True",
     incorrect_answers: ["False"],
     options: ["True", "False"],
-  },
-  {
-    category: 1, // Describe Cloud Concepts
-    type: "multiple",
-    difficulty: "medium",
-    question:
-      "Management of the cloud speaks to managing your cloud resources. In the cloud, you can: Automatically scale resource deployment based on need. Deploy resources based on a preconfigured template, removing the need for manual configuration. Monitor the health of resources and automatically replace failing resources. Receive automatic alerts based on configured metrics, so you’re aware of performance in real-time.",
-    correct_answer: "True",
-    incorrect_answers: ["False"],
-    options: ["False", "True"],
   },
   {
     category: 1, // Describe Cloud Concepts
@@ -530,6 +541,16 @@ const cloudConceptQuestions = [
     type: "multiple",
     difficulty: "medium",
     question:
+      "Horizontal Scaling is the ability to add or remove VMs or containers?",
+    correct_answer: "True",
+    incorrect_answers: ["False"],
+    options: ["True", "False"],
+  },
+  {
+    category: 1, // Describe Cloud Concepts
+    type: "multiple",
+    difficulty: "medium",
+    question:
       "What is characterized as the ability of a system to recover from failures and continue to function?",
     correct_answer: "Reliability",
     incorrect_answers: ["Predictability", "Scalability"],
@@ -540,39 +561,10 @@ const cloudConceptQuestions = [
     type: "multiple",
     difficulty: "medium",
     question:
-      "In the IaaS model, the cloud provider is responsible for the operating system installation, configuration, and maintenance; network configl database and storage config, etc.?",
+      "In the IaaS model, the cloud provider is responsible for the operating system installation, configuration, and maintenance; network config database and storage config, etc.",
     correct_answer: "False",
     incorrect_answers: ["True"],
     options: ["True", "False"],
-  },
-  {
-    category: 1, // Describe Cloud Concepts
-    type: "multiple",
-    difficulty: "medium",
-    question:
-      "Infrastructure as a service (IaaS) is the most flexible category of cloud services, as it provides you the maximum amount of control for your cloud resources.",
-    correct_answer: "True",
-    incorrect_answers: ["False"],
-    options: ["True", "False"],
-  },
-  {
-    category: 1, // Describe Cloud Concepts
-    type: "multiple",
-    difficulty: "medium",
-    question:
-      "In a PaaS environment, the cloud provider maintains the physical infrastructure, physical security, and connection to the internet. They also maintain the operating systems, middleware, development tools, and business intelligence services that make up a cloud solution.",
-    correct_answer: "True",
-    incorrect_answers: ["False"],
-    options: ["True", "False"],
-  },
-  {
-    category: 1, // Describe Cloud Concepts
-    type: "multiple",
-    difficulty: "medium",
-    question: "SaaS is the most flexible cloud model",
-    correct_answer: "True",
-    incorrect_answers: ["False"],
-    options: ["False", "True"],
   },
 ];
 
